2e33f4ff1fa169aee5945a1ccc0704ea2c8d38c5
Dan <danomaster@hotmail.co.uk>
2014-12-30 14:22:37 +0000

    All gem swords + Textures, registered

---
+++ src/main/java/oremod/com/plusmods/oresplus/ItemJadeSword.java
@@ -0,0 +1,12 @@
+package com.plusmods.oresplus;
+
+import net.minecraft.item.ItemSword;
+
+public class ItemJadeSword extends ItemSword {
+
+	public ItemJadeSword(ToolMaterial p_i45356_1_) {
+		super(p_i45356_1_);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/oremod/com/plusmods/oresplus/ItemRubySword.java
@@ -0,0 +1,12 @@
+package com.plusmods.oresplus;
+
+import net.minecraft.item.ItemSword;
+
+public class ItemRubySword extends ItemSword {
+
+	public ItemRubySword(ToolMaterial p_i45356_1_) {
+		super(p_i45356_1_);
+		// TODO Auto-generated constructor stub
+	}
+
+}
