491f76c2e081769930b3925ee86f7067d17c5241
zakdoesgaming <zakdoesgaming@gmail.com>
2014-12-31 21:50:51 +0000

    Added VanillaPlus base files and Infused Ore and Infused Dust.

---
+++ src/main/java/overhaulmod/com/plusmods/vanillaplus/items/ItemInfusedDust.java
@@ -0,0 +1,15 @@
+package com.plusmods.vanillaplus.items;//based on master condiguration
+
+import net.minecraft.item.*;
+
+public class ItemInfusedDust extends Item 
+{
+	int itemID;
+	
+	public ItemInfusedDust(int i) {
+		itemID = i;
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
